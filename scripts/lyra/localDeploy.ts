2ed8eafd69530430e697d801008821f4314dc08d
Benjamin Efigenio <befigenio@gmail.com>
2023-03-10 20:45:25 +0200

    lyra local deploy script

--- scripts/lyra/localDeploy.ts
+++ scripts/lyra/localDeploy.ts
@@ -13,27 +13,27 @@ import ERC20ABI from './helpers/abi/ERC20ABI';
 // otherwise OZ will think it's deploying to hardhat network and not local
 async function main() {
   // 1. get deployer and network
-  const provider = new ethers.providers.JsonRpcProvider('http://localhost:8545');
+  const [deployer, lyra, , , owner] = await ethers.getSigners();
 
-  const pkLocalDeployer = '0x5de4111afa1a4b94908f83103eb1f1706367c2e68ca870fc3fb9a804cdab365a';
-  const privateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'; // enter address with ETH
+  const provider = new ethers.providers.JsonRpcProvider();
+
   provider.getGasPrice = async () => {
     return ethers.BigNumber.from('0');
   };
   provider.estimateGas = async () => {
     return ethers.BigNumber.from(15000000);
-  }; // max limit to prevent run out of gas errors
-  const deployer = new ethers.Wallet(privateKey, provider);
+  };
+  // max limit to prevent run out of gas errors
 
   // 2. deploy and seed market
   const exportAddresses = true;
-  let localTestSystem = await deployTestSystem(deployer, false, exportAddresses, {
+  let localTestSystem = await deployTestSystem(lyra, false, exportAddresses, {
     mockSNX: true,
     compileSNX: false,
     optionMarketParams: { ...DEFAULT_OPTION_MARKET_PARAMS, feePortionReserved: toBN('0.05') },
   });
 
-  await seedTestSystem(deployer, localTestSystem);
+  await seedTestSystem(lyra, localTestSystem);
 
   // // 3. add new BTC market
   // let newMarketSystem = await addNewMarketSystem(deployer, localTestSystem, 'sBTC', exportAddresses)
@@ -41,16 +41,16 @@ async function main() {
 
   // 4. get global contracts
   let lyraGlobal: any = getGlobalDeploys('local');
-  // const susd = lyraGlobal.QuoteAsset.address;
-  // console.log({ susd });
-  // const susdContract = await ethers.getContractAt(ERC20ABI, susd);
-  // console.log({ susdContract });
-  // susdContract.mint
+  const susd = lyraGlobal.QuoteAsset.address;
+  const susdAbi = lyraGlobal.QuoteAsset.abi;
+
+  const susdContract = await ethers.getContractAt(susdAbi, susd);
+  await susdContract.connect(lyra).mint(deployer.address, toBN('1000000'));
+  await susdContract.connect(lyra).mint(owner.address, toBN('100000'));
   // 5. get market contracts
   let lyraMarket: any = getMarketDeploys('local', 'sETH');
   console.log('contract name:', lyraMarket.OptionMarket.contractName);
   console.log('address:', lyraMarket.OptionMarket.address);
-  // console.log("abi:", lyraMarket.OptionMarket.abi)
   console.log('bytecode:', lyraMarket.OptionMarket.bytecode.slice(0, 20) + '...');
 }
 
